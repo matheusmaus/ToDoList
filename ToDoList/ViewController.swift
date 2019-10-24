86f8ea3e1fff00e3dbb22f2aa7145ce1dced07d2
Matheus Marcos Maus <matheus.maus@gmail.com>
2019-10-24 08:48:19 -0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Matheus Marcos Maus on 24/10/19.
+//  Copyright © 2019 MXM Digital Agency. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,34 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Matheus Marcos Maus on 24/10/19.
+//  Copyright © 2019 MXM Digital Agency. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoList
+
+class ToDoListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
